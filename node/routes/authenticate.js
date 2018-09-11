38746f65ffe41a517a3746ca3d5edcc8394a7404
MINHAZ-PC\Minhaz <minhaz1217@gmail.com>
2018-09-12 00:12:13 +0600

    all working

--- node/routes/authenticate.js
+++ node/routes/authenticate.js
@@ -11,7 +11,7 @@ module.exports = function(passport){
 
 	//sends failure login state back to angular
 	router.get('/failure', function(req, res){
-		res.send({state: 'failure', user: null, message: "Invalid username or password"});
+		res.send({state: 'failure', hello:'hi', reqss: req.username, user: null, message: "Invalid username or password"});
 	});
 
 	//log in
